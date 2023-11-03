bb3aef1dc60eec87e3a3e6e7bbc330182134fc38
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2023-11-03 13:36:49 +0100

    Detailed page of the standaarden

--- components/content-footer/content-footer.vue
+++ components/content-footer/content-footer.vue
@@ -31,25 +31,25 @@
 </template>
 
 <script setup lang="ts" name="contentFooter">
-onMounted(() => {
-  const footerScript = document.createElement('script')
-  switch (import.meta.env.VITE_ENVIRONMENT) {
-    case 'Test':
-      footerScript.setAttribute(
-        'src',
-        'https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/c0df3610-36b9-4113-a487-05dfed92c317/embed',
-      )
-    case 'Production':
-      footerScript.setAttribute(
-        'src',
-        'https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/widget/f1d7f80f-ad17-4f25-92b4-027a99785068/embed',
-      )
-    default:
-      footerScript.setAttribute(
-        'src',
-        'https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/c0df3610-36b9-4113-a487-05dfed92c317/embed',
-      )
-  }
-  document.head.appendChild(footerScript)
-})
+// onMounted(() => {
+//   const footerScript = document.createElement('script')
+//   switch (import.meta.env.VITE_ENVIRONMENT) {
+//     case 'Test':
+//       footerScript.setAttribute(
+//         'src',
+//         'https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/c0df3610-36b9-4113-a487-05dfed92c317/embed',
+//       )
+//     case 'Production':
+//       footerScript.setAttribute(
+//         'src',
+//         'https://prod.widgets.burgerprofiel.vlaanderen.be/api/v1/widget/f1d7f80f-ad17-4f25-92b4-027a99785068/embed',
+//       )
+//     default:
+//       footerScript.setAttribute(
+//         'src',
+//         'https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/c0df3610-36b9-4113-a487-05dfed92c317/embed',
+//       )
+//   }
+//   document.head.appendChild(footerScript)
+// })
 </script>
---
+++ components/spotlight/spotlight.vue
@@ -0,0 +1,18 @@
+<template>
+  <article class="spotlight">
+    <h1 class="spotlight__title" v-if="title">{{ title }}</h1>
+    <h2 class="spotlight__subtitle" v-if="subtitle">{{ subtitle }}</h2>
+    <div class="spotlight__data" v-if="links?.length">
+      <div class="spotlight__data__value" v-for="link in links">
+        <a :href="link.href">{{ link?.title }}</a>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script setup lang="ts" name="spotlight">
+import type { Spotlight } from '~/types/spotlight'
+defineProps<Spotlight>()
+</script>
+
+<style scoped lang="scss" src="./style.scss" />
