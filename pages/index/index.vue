dc0457caddb3ec7a6eac8771cf38aada92d362dd
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2023-12-04 10:48:09 +0100

    Abstracted default filters into their own file for better DX

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -41,75 +41,19 @@
 import { convertQueryParams } from '~/composables/useQueryParams'
 import type { Index } from '~/types'
 import type { Standard } from '~/types/standard'
-import { FilterType, type FilterOption } from '~/types/custom-filter'
+import { type FilterOption } from '~/types/custom-filter'
+import defaultFilters from './filter.config'
 
 let selectedFilters = ref()
 
-let filters: FilterOption[] = [
-  {
-    type: FilterType.CHECKBOX,
-    title: 'Type toepassing',
-    key: 'usageType',
-    active: [false, false, false, false],
-    options: [
-      {
-        default: true,
-        label: 'Verplicht',
-        key: 'Verplicht',
-      },
-      {
-        default: false,
-        label: 'Aanbevolen (vrijwillig)',
-        key: 'Aanbevolen (vrijwillig)',
-      },
-      {
-        default: false,
-        label: 'TBD',
-        key: 'TBD',
-      },
-      {
-        default: false,
-        label: 'Pas toe of leg uit',
-        key: 'Pas toe of leg uit',
-      },
-    ],
-  },
-  {
-    type: FilterType.RADIO,
-    title: 'Type standaard',
-    key: 'category',
-    active: '',
-    options: [
-      {
-        default: '1',
-        key: 'Vocabularium',
-        label: 'Vocabularium',
-      },
-      {
-        default: '2',
-        key: 'Applicatieprofiel',
-        label: 'Applicatieprofiel',
-      },
-      {
-        default: '3',
-        key: 'Implementatiemodel',
-        label: 'Implementatiemodel',
-      },
-      {
-        default: '4',
-        key: 'All',
-        label: 'Toon alle standaarden',
-      },
-    ],
-  },
-]
+let filters: FilterOption[] = [...defaultFilters]
 
 const updateFilter = (activeFilters: Array<string[]>) => {
   selectedFilters.value = sanitizeFilters(filters, activeFilters)
 }
 
 const toggle = ref({
-  // Bit of a hacky way to call the toggleSidebar function from the sidebar component
+  // Bit of a hacky way to call the toggleSidebar function from the sidebar component. Type the event
   toggleSidebar: () => {},
 })
 
