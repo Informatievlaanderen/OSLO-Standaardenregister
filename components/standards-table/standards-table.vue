d59fc9e67da353f5d6e8d5d240a0885933c62ed4
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2023-12-05 13:42:16 +0100

    Make use of enum type for "TBD" and set default amount of standards to 20

--- components/standards-table/standards-table.vue
+++ components/standards-table/standards-table.vue
@@ -10,12 +10,12 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-if="standards?.length" v-for="standard in standards">
+      <tr v-if="standards?.length" v-for="standard in pagedDatasets()">
         <td>
           <a :href="standard?.href">{{ standard?.title }}</a>
         </td>
         <td>
-          <p>{{ standard?.category ?? 'TBD' }}</p>
+          <p>{{ standard?.category ?? Usage.TBD }}</p>
         </td>
         <td>
           <a :href="standard?.organisation?.href">{{
@@ -23,10 +23,10 @@
           }}</a>
         </td>
         <td>
-          {{ standard?.usageType ?? 'TBD' }}
+          {{ standard?.usageType ?? Usage.TBD }}
         </td>
         <td>
-          <p>{{ standard?.datePublished ?? 'TBD' }}</p>
+          <p>{{ standard?.datePublished ?? Usage.TBD }}</p>
         </td>
       </tr>
       <tr v-else>
@@ -34,15 +34,85 @@
       </tr>
     </tbody>
   </vl-data-table>
+  <vl-pager mod-align="center">
+    <vl-pager-bounds
+      v-if="standards?.length"
+      :from="paginationIndex.toString()"
+      :to="maxTo().toString()"
+      :total="standards?.length?.toString()"
+      page-label="evenementen"
+    />
+    <vl-pager-item
+      v-if="standards?.length"
+      a11yLabel="vorige"
+      label="Vorige"
+      type="previous"
+      @click="setPreviousIndex"
+    />
+    <vl-pager-item
+      v-if="standards?.length"
+      a11yLabel="volgende"
+      type="next"
+      label="Volgende"
+      @click="setNextIndex"
+    />
+  </vl-pager>
 </template>
 
 <script setup lang="ts" name="standardsTable">
-import type { Standard } from '~/types/standard'
-defineProps({
+import { Usage, type Standard } from '~/types/standard'
+const paginationIndex = ref(1)
+const itemsPerPage = 20
+const props = defineProps({
   standards: {
     type: Array<Standard>,
   },
 })
+
+const setPreviousIndex = () => {
+  const value = paginationIndex.value - itemsPerPage
+  if (value > 0) {
+    paginationIndex.value = value
+  }
+}
+
+const setNextIndex = () => {
+  if (props?.standards?.length) {
+    const value = paginationIndex.value + itemsPerPage
+    if (value <= props.standards?.length) {
+      paginationIndex.value = value
+    }
+  }
+}
+
+const maxTo = () => {
+  if (props?.standards?.length) {
+    const value = paginationIndex.value + itemsPerPage - 1
+    if (value > props?.standards?.length) {
+      return props?.standards?.length
+    } else {
+      return value
+    }
+  }
+  return 0
+}
+
+const pagedDatasets = (): Standard[] => {
+  return (
+    props?.standards?.slice(
+      paginationIndex.value - 1,
+      paginationIndex.value + itemsPerPage - 1,
+    ) ?? []
+  )
+}
+
+// reset the index when the list changes, so the first page is shown
+watch(
+  () => props?.standards,
+  () => {
+    paginationIndex.value = 1
+  },
+)
 </script>
 
 <style scoped lang="scss" src="./style.scss" />
