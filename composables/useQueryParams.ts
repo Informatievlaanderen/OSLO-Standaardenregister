0543e9a4e1fed15a8a24d4989184f29a6a0cdaba
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-05-06 16:45:53 +0200

    mapped querystring "type" to usage and renamed label from "name" to "title"

--- composables/useQueryParams.ts
+++ composables/useQueryParams.ts
@@ -1,45 +1,70 @@
-import type { LocationQuery } from "#vue-router";
-import { FilterType, type FilterOption, type SanitizedFilter } from "~/types/custom-filter";
+import type { LocationQuery } from '#vue-router'
+import {
+  FilterType,
+  type FilterOption,
+  type SanitizedFilter,
+} from '~/types/custom-filter'
 
-const updateFilter = (filter: FilterOption, indexOption: number, key: string) => {
-    let updatedFilter = { ...filter };
+const updateFilter = (
+  filter: FilterOption,
+  indexOption: number,
+  key: string,
+) => {
+  let updatedFilter = { ...filter }
 
-    if (filter.type === FilterType.RADIO) {
-        updatedFilter.active = (indexOption + 1)?.toString();
-    }
+  if (filter.type === FilterType.RADIO) {
+    updatedFilter.active = (indexOption + 1)?.toString()
+  }
 
-    if (filter.type === FilterType.CHECKBOX && Array.isArray(filter.active)) {
-        let vals: (boolean | string)[] = [...filter.active];
-        vals.splice(indexOption, 1, true);
-        updatedFilter.active = vals;
-    }
+  if (filter.type === FilterType.CHECKBOX && Array.isArray(filter.active)) {
+    let vals: (boolean | string)[] = [...filter.active]
+    vals.splice(indexOption, 1, true)
+    updatedFilter.active = vals
+  }
 
-    return updatedFilter;
-};
+  return updatedFilter
+}
 
-export const convertQueryParams = (query: LocationQuery, filters: FilterOption[]): {
-    sanitized: SanitizedFilter,
-    filters: FilterOption[],
+export const convertQueryParams = (
+  query: LocationQuery,
+  filters: FilterOption[],
+): {
+  sanitized: SanitizedFilter
+  filters: FilterOption[]
 } => {
-    let updatedFilters: FilterOption[] = [...filters];
-    let sanitizedFilter: SanitizedFilter = {};
+  let updatedFilters: FilterOption[] = [...filters]
+  let sanitizedFilter: SanitizedFilter = {}
 
-    for (let key in query) {
-        let filter = filters.find((filter) => filter.key === key);
-
-        if (filter) {
-            let index = filters?.indexOf(filter);
-            let indexOption = filter?.options.findIndex((option) => option?.key?.toLowerCase() === query[key]?.toString()?.toLowerCase());
-
-            if (index >= 0 && indexOption >= 0 && key) {
-                sanitizedFilter = { ...sanitizedFilter, [key]: [filter?.options[indexOption].key] };
-                updatedFilters[index] = updateFilter(filter, indexOption, key);
-            }
-        }
+  for (let key in query) {
+    // Hardcoded fix to map type to usage. The label in the table from the fronend is 'type' but the key in the filter is 'usage'
+    const value = query[key]
+    if (key === 'type') {
+      key = 'usage'
     }
+    console.log(key)
+    console.log(key)
+    console.log(key)
+    let filter = filters.find((filter) => filter.key === key)
 
-    return {
-        sanitized: sanitizedFilter,
-        filters: updatedFilters,
-    };
+    if (filter) {
+      let index = filters?.indexOf(filter)
+      let indexOption = filter?.options.findIndex(
+        (option) =>
+          option?.key?.toLowerCase() === value?.toString()?.toLowerCase(),
+      )
+
+      if (index >= 0 && indexOption >= 0 && key) {
+        sanitizedFilter = {
+          ...sanitizedFilter,
+          [key]: [filter?.options[indexOption].key],
+        }
+        updatedFilters[index] = updateFilter(filter, indexOption, key)
+      }
+    }
+  }
+
+  return {
+    sanitized: sanitizedFilter,
+    filters: updatedFilters,
+  }
 }
