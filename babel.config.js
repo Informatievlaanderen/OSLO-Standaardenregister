a376b1297fb07a9e1c4474537bc18abab5de213f
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-08-21 11:02:30 +0200

    Added `datePublicReviewStart`, `datePublicReviewEnd` and `endOfPublicationDate` to the detail page

--- babel.config.js
+++ babel.config.js
@@ -2,22 +2,22 @@
 // Needed for Vite to be able to correctly export modules using import.meta
 
 module.exports = {
-    presets: [
-        [
-            '@babel/preset-env',
-            { useBuiltIns: 'entry', corejs: '2', targets: { node: 'current' } },
-        ],
-        '@babel/preset-typescript',
+  presets: [
+    [
+      '@babel/preset-env',
+      { useBuiltIns: 'entry', corejs: '2', targets: { node: 'current' } },
     ],
-    plugins: [
-        function () {
-            return {
-                visitor: {
-                    MetaProperty(path) {
-                        path.replaceWithSourceString('process')
-                    },
-                },
-            }
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    function () {
+      return {
+        visitor: {
+          MetaProperty(path) {
+            path.replaceWithSourceString('process')
+          },
         },
-    ],
+      }
+    },
+  ],
 }
