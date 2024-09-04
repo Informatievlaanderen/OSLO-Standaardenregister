98f842537cda4623115b9ef947802b41c7fa2722
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-09-04 16:11:08 +0200

    Updated POC of translations in standaardenregister

--- types/custom-filter.ts
+++ types/custom-filter.ts
@@ -1,25 +1,25 @@
-import type { Modal } from "~/types/custom-modal";
+import type { Modal } from '~/types/custom-modal'
 
 export interface FilterOption {
-    type: FilterType,
-    title?: string,
-    key: string,
-    active: Array<string | boolean> | string,
-    options: FilterValue[],
-    modal?: Modal,
+  type: FilterType
+  title?: string
+  key: string
+  active: Array<string | boolean> | string
+  options: FilterValue[]
+  modal?: Modal
 }
 
 export enum FilterType {
-    RADIO = "radio",
-    CHECKBOX = "checkbox",
+  RADIO = 'radio',
+  CHECKBOX = 'checkbox',
 }
 
 export interface FilterValue {
-    default: string | boolean,
-    label: string,
-    key: string,
+  default: string | boolean
+  label: string
+  key: string
 }
 
 export interface SanitizedFilter {
-    [key: string]: Array<string>,
+  [key: string]: Array<string>
 }
