b7a40bdeb37132cd6e41067b92c059a772c2574e
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-09-10 10:41:05 +0200

    Added `try/catch` around text transformations so that it does not break the page

--- composables/useTextTransformations.ts
+++ composables/useTextTransformations.ts
@@ -1,7 +1,17 @@
 export const useCapitalizeFirstLetter = (string: string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  try {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  } catch (err) {
+    console.error(err)
+    return string
+  }
 }
 
 export const useRemoveDashes = (string: string) => {
-    return string.replace(/-/g, ' ');
+  try {
+    return string.replace(/-/g, ' ')
+  } catch (err) {
+    console.error(err)
+    return string
+  }
 }
