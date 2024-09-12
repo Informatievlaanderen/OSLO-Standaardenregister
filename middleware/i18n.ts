fdc4c7bdb14dae08e832b68cfd1683b0ea016486
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-09-12 13:35:59 +0200

    Added fallback for incorrect cookie values in middleware

--- middleware/i18n.ts
+++ middleware/i18n.ts
@@ -1,7 +1,24 @@
+import { validateLocaleCookie } from '~/utils/i18n.utils'
+
 export default defineNuxtRouteMiddleware(async () => {
-
   const nuxtApp = useNuxtApp()
-  const cookie = useCookie('i18n_redirected');
+  const cookie = useCookie('i18n_redirected')
 
-  await nuxtApp.$i18n.setLocale(cookie.value || nuxtApp.$i18n.defaultLocale)
+  if (!cookie.value) return
+
+  // Validate the locale cookie
+  const validLocale = validateLocaleCookie(
+    cookie.value,
+    nuxtApp.$i18n.defaultLocale,
+    nuxtApp.$i18n.availableLocales,
+  )
+
+  // Set the locale to the valid locale or default locale
+  const localeToSet = validLocale || nuxtApp.$i18n.defaultLocale
+
+  try {
+    await nuxtApp.$i18n.setLocale(localeToSet)
+  } catch (error) {
+    console.error('Error setting locale:', error)
+  }
 })
