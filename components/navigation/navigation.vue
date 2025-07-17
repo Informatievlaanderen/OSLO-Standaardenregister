a955d3693004a375aedd56ad29d70ffa1d40a956
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2025-07-17 13:46:39 +0200

    Styling tweaks

--- components/navigation/navigation.vue
+++ components/navigation/navigation.vue
@@ -1,16 +1,16 @@
 <template>
-  <vl-link-list mod-inline>
-    <vl-link-list-item>
+  <vl-link-list mod-inline class="navigation">
+    <vl-link-list-item class="navigation-item">
       <vl-link href="/standaarden" mod-block mod-bold>
         {{ $t('standards') }}
       </vl-link>
     </vl-link-list-item>
-    <vl-link-list-item>
+    <vl-link-list-item class="navigation-item">
       <vl-link href="/standaarden/statistieken" mod-block mod-bold>
         {{ $t('statistics') }}
       </vl-link>
     </vl-link-list-item>
-    <vl-link-list-item>
+    <vl-link-list-item class="navigation-item">
       <vl-link href="/standaarden/tijdlijn" mod-block mod-bold>
         {{ $t('timeline') }}
       </vl-link>
